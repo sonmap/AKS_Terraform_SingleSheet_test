a032033832c8addc8d4eaa47b03c64dcfc37b83e
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-06-27 08:25:37 +0900

    Add k8s app module variables

---
+++ modules/k8s-app/variables.tf
@@ -0,0 +1,14 @@
+variable "apps" {
+  description = "Application map generated from the single-sheet design."
+  type = map(object({
+    enabled        = bool
+    app_type       = string
+    namespace      = string
+    image          = string
+    container_port = number
+    replicas       = number
+    service_type   = string
+    service_port   = number
+  }))
+  default = {}
+}
